7292977f6e47bb7dde2e8f7d180581310867e814
marskib <tata.marcina@autyzmsoft.pl>
2017-08-17 08:57:45 +0200

    ScrollView na dialogModalny + wymiarowanie

--- app/src/main/java/autyzmsoft/pl/profmarcin/DialogModalny.java
+++ app/src/main/java/autyzmsoft/pl/profmarcin/DialogModalny.java
@@ -2,7 +2,15 @@ package autyzmsoft.pl.profmarcin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+
+/**
+ * Wyswietla okienko modalne.
+ * Zrodlo - stackoverflow
+ * Dotakowo jest jeszcze potrzebny wpis w manifest.xml:
+ * action android:name="autyzmsoft.pl.profmarcin.DialogModalny"/>   --> it gives the activity the dialog look...
+ */
 
 public class DialogModalny extends Activity {
 
@@ -13,16 +21,26 @@ public class DialogModalny extends Activity {
         setFinishOnTouchOutside (false);  //to make it behave like a modal dialog
 
         setContentView(R.layout.activity_dialog_modalny);
-    }
 
+
+
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int szer = displaymetrics.widthPixels;
+        View layoutSki = findViewById(R.id.sv_DialogModalny);
+        layoutSki.getLayoutParams().width = (int) (szer*0.70f);
+        layoutSki.requestLayout(); //teraz nastepuje zaaplikowanie zmian
+
+    }
 
 
     @Override public void onBackPressed() { //to make it behave like a modal dialog
         // prevent "back" from leaving this activity
+        //zwroc uwage, ze tutaj nie ma super() - nadpisanie/zlikwidowanie metody macierzystej i potomnej
      }
 
      public void onClickbtnOK(View v) {
-         //zamkniecie activity, zeby przejsc do MainActivity
+         //zamkniecie activity, zeby przejsc do MainActivity (wywolywacza)
          finish();
      }
 
